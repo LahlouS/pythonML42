c98feb56b921d49975f6a4e6524e959563737bd9
lahlouSacha <lahlousacha@gmail.com>
2023-06-27 19:53:14 +0200

    ex09 OK

--- ex09/guess.py
+++ ex09/guess.py
@@ -1,43 +1,38 @@
 import random
 
-secretNum = random.randint(1, 100)
-print(secretNum)
+secret_num = random.randint(1, 99)
+guess = None
+
 count = 0
-
-
-def verify(a, b):
-    global count
-    if a == b and b == 42:
-        print('The answer to the ultimate question of life, the universe and everything is 42.')
-    if a < b:
-        print('too low')
-        count += 1
-    elif a > b:
-        print('too high')
-        count += 1
-    elif a == b and count == 0:
-        print('well done, you got it first try')
-        exit()
-    elif a == b:
-        print('well done, you got it at attempt {}'.format(count))
-        exit()
-
-
 
 print('''
     This is an interactive guessing game!
     You have to enter a number between 1 and 99 to find out the secret number.
     Type 'exit' to end the game.
-    Good luck !
-''')
+    Good luck!
+    ''')
 
-while 1:
-    raw = input('What\'s your guess between 1 and 99? ')
+while guess != 'exit':
+    guess = input('Choose a number from 1 to 100: ')
     try:
-        verify(int(raw), secretNum)
-    except:
-        if raw == 'exit':
-            print('exiting. . . ')
-            exit()
+        nb = int(guess)
+        if nb < secret_num:
+            print('--> too low')
+        elif nb > secret_num:
+            print('--> too high')
         else:
-            print('this is not an integer')
+            guess = 'exit'
+            if nb == 42:
+                print('The answer to the ultimate question of life, the universe and everything is 42.')
+            if count == 0:
+                print('Congratulation, you found it first time !')
+            else:
+                print('Congrats, you found it\nYou won in {} attemps'.format(count))
+        count += 1
+    except:
+        if guess != 'exit':
+            print('--> input error')
+
+print('\nGoodbye')
+
+
