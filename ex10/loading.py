32047065d7c63231f608a3d1c8ab4e23535e55bd
lahlouSacha <lahlousacha@gmail.com>
2023-06-30 19:34:27 +0200

    exo 10 OK

--- ex10/loading.py
+++ ex10/loading.py
@@ -2,15 +2,24 @@ import time
 
 def ft_progress(listy):
     i = 0
-    past = int(time.time())
+    start_time = time.time()
+    diff = 0
     while i < len(listy):
-        print('time --> ', int(time.time()) - past)
-        past = int(time.time())
-        print('')
+        progress_bar = '=' * int(i / len(listy) * 20) + '>'
+        formatted_output = 'ETA: %.2fs [%d%s][%s] %d/%d | elapsed time %.3f' %((time.time() - (start_time + diff)) * len(listy), i / len(listy) * 100, '%', progress_bar.ljust(21, ' '), i, len(listy), time.time() - start_time)
+        if (len(listy) - 1 != i):
+            print(formatted_output, end='\r')
+        else:
+            print(formatted_output, end='\n')
+        diff = time.time() - start_time
         yield listy[i]
+        e = 0
         i += 1
 
-for i in ft_progress([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]):
-    print('main --> ', i)
-    time.sleep(1)
-
+listy = range(1000)
+ret = 0
+for elem in ft_progress(listy):
+    ret += (elem + 3) % 5
+    time.sleep(0.01)
+print()
+print(ret)
