34292776d7ca29e574652a6a84afbd0d4d05a35d
lahlouSacha <lahlousacha@gmail.com>
2023-07-13 19:39:00 +0200

    ex02 seems ok but not tested yet

--- module01/ex02/test.py
+++ module01/ex02/test.py
@@ -17,10 +17,43 @@ print(v3.values)
 print(v3.shape)
 
 print('\n\n--------------v1 dot v2---------------')
-
-print(v1.dot(v2))
-
+print('Vector:', v1.dot(v2))
 test1 = np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
 test2 = np.array([10.0, 11.0, 12.0, 13.0, 14.0, 15.0])
+print('Numpy:', np.dot(test1, test2))
 
-print(np.dot(test1, test2))
+print('\n\n--------------switching v1, v2, v3 shape---------------')
+
+print('initial values v1:', v1.values, 'after switch values v1:', v1.T().values)
+print('initial values v2:', v2.values, 'after switch values v2:', v2.T().values)
+print('initial values v3:', v3.values, 'after switch values v3:', v3.T().values)
+
+print('\n\n--------------__add__ v1, v2---------------')
+v12 = v1 + v2
+
+print(v12.values)
+
+print('\n\n--------------__sub__ v1, v2---------------')
+v12 = v1 - v2
+print(v12)
+
+print('\n\n------creating new vectors v4 v5 in row shape----------')
+v3 = Vector([[1, 2, 3]])
+v4 = Vector([[4, 5, 6]])
+print(v3.values)
+print(v4.values)
+
+
+print('\n\n------testing dot product----------')
+print('v3.dot(v4):', v3.dot(v4))
+np3 = np.array([1, 2, 3], float)
+np4 = np.array([4, 5, 6], float)
+print('np.dot(np3, np4):', np.dot(np3, np4))
+
+print('\n\n------testing __mul__----------')
+print('v4 * v3.dot(v4):', v4 * v3.dot(v4))
+print('np4 * np.dot(np3, np4):', np4 * np.dot(np3, np4))
+
+print('\n\n------testing __truediv__----------')
+print('v4 / v3.dot(v4):', v4 / v3.dot(v4))
+print('np4 / np.dot(np3, np4):', np4 / np.dot(np3, np4))
--- module01/ex02/vector.py
+++ module01/ex02/vector.py
@@ -7,6 +7,10 @@ import numpy as np
     # FINISH OTHER OPERATION
     # TEST
     
+
+    # (1, n) = [[0,1,2,3,4, . . .]]
+    # (n, 1) = [[1], [2], [3], . . .]
+
 class Vector:
     def __init__(self, arg):
         try:
@@ -14,22 +18,22 @@ class Vector:
             if isinstance(arg, list):
                 if len(arg) == 1 and isinstance(arg[0], list):
                     self.values.append([float(val) for val in arg[0]])
-                    self.shape = (1, len(arg[0]))
+                    self.shape = (1, len(arg[0])) # (1, n) is row vector aka TYPE 2 (see __typeChecking() method)
                 elif len(arg) != 0:
                     for val in arg:
-                        if not isinstance(val, list) or len(val) != 1 or isinstance(val[0], float):
+                        if not isinstance(val, list) or len(val) != 1:
                             raise TypeError('Column vectors values must be lists of single float')
                         else:
                             self.values.append([float(val[0])])
                     self.values = arg
-                    self.shape = (len(arg), 1)
+                    self.shape = (len(arg), 1) # (n, 1) is columns vector aka TYPE 1 (see __typeChecking() method)
             elif isinstance(arg, int):
                 self.values = [[float(x)] for x in range(arg)]
-                self.shape = (len(self.values), 1)
+                self.shape = (len(self.values), 1) # (n, 1) is columns vector aka TYPE 1 (see __typeChecking() method)
             elif isinstance(arg, tuple) and len(arg) == 2:
                 if arg[0] < arg[1]:
                     self.values = [[float(x)] for x in range(arg[0], arg[1])]
-                    self.shape = (len(self.values), 1)
+                    self.shape = (len(self.values), 1) # (n, 1) is columns vector aka TYPE 1 (see __typeChecking() method)
                 else:
                     raise TypeError('unvalid range')
             else:
@@ -38,28 +42,125 @@ class Vector:
             print('ERROR:', t)
             sys.exit()
 
-    def typeChecking(self, V):
-        if self.shape[0] == 1 and V.shape[0] == 1 and self.shape[1] == V.shape[1]:
+    def __typeChecking(self, V):
+        if not isinstance(V, Vector):
+            raise TypeError('arg is not of type Vector')
+        if self.shape == V.shape:
+            if self.shape[0] == 1:
+                return 2
             return 1
-        if self.shape[1] == 1 and V.shape[1] == 1 and self.shape[0] == V.shape[0]:
-            return 2
         return 0
     
+    def __atIndex(self, i):
+        if self.__typeChecking(self) == 2:
+            return self.values[0][i]
+        else:
+            return self.values[i][0]
 
-
-    def __iterateVector(self, v_b):
+    def __iterateVector(self, v_b, vecType):
         i = 0
-        while i < self.shape[0]:
-            if self.typeChecking(v_b) == 1:
-                yield self.values[0][i], v_b.values[0][i]
-            elif self.typeChecking(v_b) == 2:
-                yield self.values[i][0], v_b.values[i][0]
-            elif not self.typeChecking(v_b):
-                raise
+        while i < self.shape[vecType - 1]: # type 2, size is at index 1 type 1, size is at index 0
+            yield self.__atIndex(i), v_b.__atIndex(i)
             i += 1
 
     def dot(self, V):
         scalar = 0
-        for v1, v2 in self.__iterateVector(V):
-            scalar += v1 * v2
-        return scalar
+        try:
+            vectorType = self.__typeChecking(V)
+            if vectorType:
+                for v1, v2 in self.__iterateVector(V, vectorType):
+                    scalar += v1 * v2
+                return scalar
+            else:
+                raise ValueError('Vectors types are not compatible !')
+        except (TypeError, ValueError) as v:
+            print('ERROR', v)
+
+    # (1, n) == [[0,1,2,3,4, . . .]] == type 2
+    # (n, 1) == [[1], [2], [3], . . .] == type 1
+
+    def T(self):
+        if self.__typeChecking(self) == 2:
+            return Vector([[val] for val in self.values[0]])
+        else:
+            return Vector([[val[0] for val in self.values]])
+   
+   
+    # the dunders
+    def __add__(self, y):
+        try:
+            vectorType = self.__typeChecking(y)
+            if vectorType == 1:
+                return Vector( [[val1 + val2] for val1, val2, in self.__iterateVector(y, vectorType)])
+            elif vectorType == 2:
+                return Vector([[val1 + val2 for val1, val2, in self.__iterateVector(y, vectorType)]])
+            else: 
+                raise ValueError('Vectors types are not compatible !')
+        except (TypeError, ValueError) as t:
+            print('ERROR:', t)
+    
+    def __radd__(self, y):
+        if not isinstance(y, Vector):
+            raise NotImplementedError
+        else:
+            self.__add__(y)
+
+    def __sub__(self, y):
+        try:
+            vectorType = self.__typeChecking(y)
+            if vectorType == 1:
+                return Vector( [[val1 - val2] for val1, val2, in self.__iterateVector(y, vectorType)])
+            elif vectorType == 2:
+                return Vector([[val1 - val2 for val1, val2, in self.__iterateVector(y, vectorType)]])
+            else: 
+                raise ValueError('Vectors types are not compatible !')
+        except (TypeError, ValueError) as t:
+            print('ERROR:', t)
+
+    def __rsub__(self, y):
+        if not isinstance(y, Vector):
+            raise NotImplementedError
+        else:
+            self.__sub__(y)
+
+    def __truediv__(self, y):
+        try:
+            yOk = float(y)
+            vectorType = self.__typeChecking(self)
+            if vectorType == 1:
+                return Vector( [ [val1[0] / yOk] for val1 in self.values ])
+            elif vectorType == 2:
+                return Vector([ [val1 / yOk for val1 in self.values[0]] ])
+            else: 
+                raise ValueError('Vectors types are not compatible !')
+        except (TypeError, ValueError) as t:
+            print('ERROR:', t)
+
+    def __rtruediv__(self, y):
+        raise NotImplementedError
+
+    def __mul__(self, y):
+        try:
+            yOk = float(y)
+            vectorType = self.__typeChecking(self)
+            if vectorType == 1:
+                return Vector( [ [val1[0] * yOk] for val1 in self.values ])
+            elif vectorType == 2:
+                return Vector([ [val1 * yOk for val1 in self.values[0]] ])
+            else: 
+                raise ValueError('Vectors types are not compatible !')
+        except (TypeError, ValueError) as t:
+            print('ERROR:', t)
+
+    def __rmul__(self, y):
+        try:
+            self.__mul__(float(y))
+        except:
+            raise NotImplementedError
+
+    def __str__(self):
+        return f'Vector of type {str(self.shape)}\nvalues are:\n{str(self.values)}'
+    def __repr__(self):
+        return f'Vector of type {str(self.shape)}\nvalues are:\n{str(self.values)}'
+
+    
