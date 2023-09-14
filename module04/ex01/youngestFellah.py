76ef60a5f43104f2525efa2c928403e7387f2071
lahlouSacha <lahlousacha@gmail.com>
2023-09-14 18:07:25 +0200

    ex00 ex01 seems ok

---
+++ module04/ex01/youngestFellah.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from fileLoader import FileLoader
+
+def youngestFellah(df, year):
+    if isinstance(df, pd.DataFrame) and isinstance(year, int):
+
+        m_filtered_df = df[(df['Sex'] == 'M') & (df['Year'] == year)]
+        f_filtered_df = df[(df['Sex'] == 'F') & (df['Year'] == year)]
+        
+        try:
+            sorted_df = {
+                        "m": m_filtered_df.sort_values(by='Age')["Age"].iloc[0],
+                        "f": f_filtered_df.sort_values(by='Age')["Age"].iloc[0]
+                        }
+        except Exception as err:
+            print(f'ERRROR: {err} (which basically means that the year you are looking for does not exist)')
+            sorted_df = None
+        return sorted_df
+
+
+if __name__ == "__main__":
+    fl = FileLoader()
+    olymp_df = fl.load('athlete_events.csv')
+    print(youngestFellah(olymp_df, 1992))
